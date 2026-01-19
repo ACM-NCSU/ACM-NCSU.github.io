82295abcc396ded767c04a436cdc7668a8da8fed
Nicholas Sutton <nicksutton46@gmail.com>
2026-01-18 22:47:48 -0500

    [Fix] Updated headerlink to use correct home link

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ACM @ NCSU';
-export const SITE_DESCRIPTION = 'NCSU ACM Website';
+export const SITE_TITLE = 'ACM@NCSU';
+export const SITE_DESCRIPTION = 'ACM@NCSU Website';
